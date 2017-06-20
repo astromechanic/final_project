e834b9e21342587ae8b41e8354fd0a45403e2261
astromechanic <violett.ivanova@gmail.com>
2017-06-20 20:52:05 +0300

    Update crawler_and_flask_ver2.py

--- crawler_and_flask_ver2.py
+++ crawler_and_flask_ver2.py
@@ -209,4 +209,7 @@ def drawing_graph(d_nouns):
    
  
 if __name__ == '__main__':
-    app.run()
+    import os
+    app.debug = True
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
